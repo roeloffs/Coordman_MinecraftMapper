c2dae31c44624501f6cda35f30366a70b47c2283
RulesOff <rulesoff2b2t@gmail.com>
2025-11-14 20:52:30 +0100

    Better toggleable coordinates display, removed default tile layers

--- coordman.js
+++ coordman.js
@@ -1,108 +1,79 @@
 let attribution = '<a href="https://github.com/rebane2001/coordman">Coordman</a> by Rebane';
 let tileSize = 512;
 
-// Layers for different dimensions
-let tileLayers = {
-"nether": L.tileLayer(
-  'tiles/{DIM}/{z}/{x},{y}.png', {
-    attribution,
-    tileSize,
-    maxZoom: 0,
-    minZoom: -16,
-    DIM: "DIM-1"
-  }),
-
-"netherbg": L.tileLayer(
-  'tiles/{DIM}/{z}/{x},{y}.png', {
-    attribution,
-    tileSize,
-    maxZoom: 0,
-    minZoom: -16,
-    zoomOffset: 3,
-    DIM: "DIM-1"
-  }),
-
-"overworld": L.tileLayer(
-  'tiles/{DIM}/{z}/{x},{y}.png', {
-    attribution,
-    tileSize,
-    maxZoom: 0,
-    minZoom: -16,
-    DIM: "DIM0"
-  }),
-
-
-"end": L.tileLayer(
-  'tiles/{DIM}/{z}/{x},{y}.png', {
-    attribution,
-    tileSize,
-    maxZoom: 0,
-    minZoom: -16,
-    DIM: "DIM1"
-  })
-};
-
-let layers = {
-    "Overworld+Nether": L.layerGroup([tileLayers['netherbg'],tileLayers['overworld']]),
-    "Overworld": tileLayers['overworld'],
-    "Nether": tileLayers['nether'],
-    "The End": tileLayers['end']
-};
-
 // Guide overlays
 let worldBorder = L.polyline([
-    [-30000000, -30000000],
-    [-30000000, 30000000],
-    [30000000, 30000000],
-    [30000000, -30000000],
-    [-30000000, -30000000]
+  [-30000000, -30000000],
+  [-30000000, 30000000],
+  [30000000, 30000000],
+  [30000000, -30000000],
+  [-30000000, -30000000]
 ], {color: 'red'});
 
 let mainHighways = L.polyline([
-    [0, 0],
-    [-30000000, 0],
-    [0, 0],
-    [30000000, 0],
-    [0, 0],
-    [0, -30000000],
-    [0, 0],
-    [0, 30000000],
-    [0, 0]
+  [0, 0],
+  [-30000000, 0],
+  [0, 0],
+  [30000000, 0],
+  [0, 0],
+  [0, -30000000],
+  [0, 0],
+  [0, 30000000],
+  [0, 0]
 ], {color: 'LimeGreen'});
 
 let diagHighways = L.polyline([
-    [0, 0],
-    [-30000000, -30000000],
-    [0, 0],
-    [30000000, -30000000],
-    [0, 0],
-    [-30000000, 30000000],
-    [0, 0],
-    [30000000, 30000000],
-    [0, 0]
+  [0, 0],
+  [-30000000, -30000000],
+  [0, 0],
+  [30000000, -30000000],
+  [0, 0],
+  [-30000000, 30000000],
+  [0, 0],
+  [30000000, 30000000],
+  [0, 0]
 ], {color: 'MediumSpringGreen'});
 
 let disabledLayer = L.tileLayer("",{minZoom: 99});
 let waypoints = {
-    "- Guides -": disabledLayer,
-    "Worldborder": worldBorder,
-    "Main Highways": mainHighways,
-    "Diag. Highways": diagHighways
+  "- Guides -": disabledLayer,
+  "Worldborder": worldBorder,
+  "Main Highways": mainHighways,
+  "Diag. Highways": diagHighways
 };
 
-let defaultLayers = [tileLayers['overworld'], worldBorder];
+let defaultLayers = [worldBorder, mainHighways, diagHighways];
 
 // Create the map
 var map = L.map('map', {
-    crs: L.CRS.Simple,
-    minZoom: -16,
-    layers: defaultLayers
+  crs: L.CRS.Simple,
+  minZoom: -16,
+  layers: defaultLayers
 }).setView([0,0], 0);
 
-L.control.layers(layers, waypoints).addTo(map);
+L.control.layers(null, waypoints).addTo(map);
 
 // Set coord overlay on the bottom
-map.on('mousemove', function(ev) {
-    let coords = [Math.round(ev.latlng.lng),Math.round(-ev.latlng.lat)];
-    document.getElementById("coordstext").innerText = "Coords: " + coords;
+map.on('mousemove', function (ev) {
+  let coords = [Math.round(ev.latlng.lng), Math.round(-ev.latlng.lat)];
+  let coordsN = [Math.round(ev.latlng.lng / 8), Math.round(-ev.latlng.lat / 8)];
+  document.getElementById("coordstext").innerText = "Coords: " + numberSeperator(coords) + "\r\nNether: " + numberSeperator(coordsN) + "";
 });
+
+function numberSeperator(num) {
+  num = "X: " + num.toString().replace(",", "   Z: ")
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+function toggleCoords() {
+  let displayStyle = document.getElementById("coordstext").style.display == "none" ? "block" : "none";
+  document.getElementById("coordstext").style.display = displayStyle;
+}
+
+function openLayers() {
+  window.chrome.webview.postMessage("OpenLayers");
+}
+
+function openSettings() {
+  window.chrome.webview.postMessage("OpenSettings");
+}
