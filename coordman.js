da076a1d8c3b8497de154dad9e5b9ea064f64502
RulesOff <rulesoff2b2t@gmail.com>
2025-11-14 20:08:07 +0100

    Cleanup

--- coordman.js
+++ coordman.js
@@ -1,9 +1,3 @@
-if (!("userdata" in window))
-  alert("Userdata corrupted or not found, please consult console logs and the README!")
-
-let openSidebar = () => document.getElementById("sidebar").style.width = "350px";
-let closeSidebar = () => document.getElementById("sidebar").style.width = "0";
-
 let attribution = '<a href="https://github.com/rebane2001/coordman">Coordman</a> by Rebane';
 let tileSize = 512;
 
@@ -93,40 +87,10 @@ let waypoints = {
     "- Guides -": disabledLayer,
     "Worldborder": worldBorder,
     "Main Highways": mainHighways,
-    "Diag. Highways": diagHighways,
-    "- <a href=\"#\" onClick=\"openSidebar()\">Waypoints</a> -": disabledLayer,
+    "Diag. Highways": diagHighways
 };
 
 let defaultLayers = [tileLayers['overworld'], worldBorder];
-
-let sidebar = document.getElementById("sidebar");
-
-// Load icons/locations from userdata
-for (const group of window.userdata.groups){
-  let groupItems = [];
-  sidebar.innerHTML += `<h3>${group.name}</h3>`
-  for (const marker of group.markers){
-    let icon = L.icon({
-      iconUrl: `icons/${marker.icon}.png`,
-      shadowUrl: 'icons/marker-shadow.png',
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      tooltipAnchor: [16, -28],
-      shadowSize: [41, 41]
-    });
-    // Leaflet maps use a weird coordinate system, so this is fine
-    let newMarker = L.marker([-marker.coords[1], marker.coords[0]], {icon});
-    let markerData = (typeof marker.data == "string") ? marker.data : marker.data.join("<br>\n");
-    newMarker.bindPopup(`<h3>${marker.name}</h3>${markerData}`);
-    groupItems.push(newMarker);
-    console.log(newMarker.getLatLng());
-    sidebar.innerHTML += `${marker.name} (<a href="#" onClick="map.setView([${-marker.coords[1]},${marker.coords[0]}],0)">${marker.coords}</a>)<br>`;
-  }
-  waypoints[group.name] = L.layerGroup(groupItems);
-  if (group.default)
-    defaultLayers.push(waypoints[group.name]);
-}
 
 // Create the map
 var map = L.map('map', {
